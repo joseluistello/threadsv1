f73077159ed09f1a7148522d0339d65664848ce2
joseluistello <jltello96@gmail.com>
2023-08-03 20:36:36 -0600

    adds

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/dist/client/components/headers'
 import AuthButtonServer from './auth-button-server';
 import { redirect } from 'next/navigation';
+import NewTweet from './new-tweet';
 
 
 export default async function Home() {
@@ -15,12 +16,13 @@ export default async function Home() {
 
 
   const { data: tweets } = await supabase.from("tweets").
-select();
+select("*, profiles(*)");
 
 
   return (
     <>
     <AuthButtonServer/>
+    <NewTweet/>
     <pre>{JSON.stringify(tweets, null, 2)}</pre>
     </>
   )
